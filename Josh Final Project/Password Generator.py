b35f3ff05ab146ba3c6e58ae806beba1f9f700b8
Elvis Sunday <75866932+elvancedonzy@users.noreply.github.com>
2022-05-17 12:25:27 -0400

    Josh Project added

---
+++ Josh Final Project/Password Generator.py
@@ -0,0 +1,23 @@
+#Importing the relevant modules
+import qrcode
+from random import randint
+
+#All uppercase password
+password = ""
+for i in range(4):
+  i = chr(randint(65, 90))
+#Adds lower case
+  for j in range(4):
+    j = chr(randint(65, 90)).lower()
+#Adds numbers 
+    for k in range(3):
+      k = randint(0, 10)
+  password = str(password) + i + j + str(k)
+print(password)
+
+#  -------------> Second Part of project <----------------
+#Incode the password and turn it to a qrcode 
+img =qrcode.make(password)
+img.save("password.jpg")
+
+
