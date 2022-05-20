be76c065b31b3a44491bc9160c2e82eaa095eb21
Elvis Sunday <75866932+elvancedonzy@users.noreply.github.com>
2022-05-20 09:37:03 -0400

    Added Nielsen Interview

---
+++ Nielsen Interview/Add Number Series.py
@@ -0,0 +1,14 @@
+#Create a sum count
+sum=0
+#Ask user for input
+y=int(input())
+#Creates a range between 0 and n number
+for x in range (0,y+1):
+#Skips number divisible by 5
+  if x%5!=0:
+#Skips number divisible by 7
+    if x%7!=0:
+#Sums up number
+      sum=sum+x
+#Prints out sum
+print("Sum of number", sum)
---
+++ Nielsen Interview/Readme.m
@@ -0,0 +1,20 @@
+Add Number Series
+
+Programming challenge description:
+
+Write a program that, given an integer, sums all the numbers from 1 through that integer (both inclusive). Do not include in your sum any of the intermediate numbers (1 and n inclusive) that are divisible by 5 or 7.
+
+Input:
+Your program should read lines from standard Input. Each line contains a positive integer.
+
+Output:
+For each line of input, print to standard output the sum of the numbers from 1 through n, disregarding those divisible by 5 and 7. Print out each result on a new line.
+
+Test 1
+Test Input: 10
+Expected Output: 33
+
+
+Test 2
+Test Input: 7
+Expected Output: 16
